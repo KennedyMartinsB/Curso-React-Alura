88450261b95db0ff7dcf17bb1eb6c9bb3cd1fa96
KennedyMartinsB <kenedy.martins185@gmail.com>
2025-02-06 18:13:01 -0300

    Feature: styled component

--- alura-books/src/components/Header/index.js
+++ alura-books/src/components/Header/index.js
@@ -3,15 +3,22 @@
 import OpcoesHeader from '../OpcoesHeader'
 import IconesHeader from '../IconesHeader'
 import Logo from '../Logo'
-import './style.css'
+// import './style.css'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+  background-color: #FFF;
+  display: flex;
+  justify-content: center;
+`
 
 function Header() {
     return (
-        <header className='App-header'>
+        <HeaderContainer>
         <Logo/>
         <OpcoesHeader/>
         <IconesHeader/>
-      </header>
+      </HeaderContainer>
     )
 }
 
